2d67eeedb7c616308d61ee2043530baf736705e2
Max Yu <carlyou0815@gmail.com>
2024-03-18 12:03:30 +0800

    Calculation

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+# Max Yu
+# Calulator to add two numbers and display the result
+
+
+sum = 0 
+
+# 1. Input
+x1 = input("Type x1: ")
+x2 = input("Type x2: ")
+op = input("Enter operator: ")
+
+# 2. Process
+if op == "+":
+    sum = int(x1) + int(x2)
+elif op == "-":
+    sum = int(x1) - int(x2)
+elif op == "*":
+    sum = int(x1) * int(x2)
+elif op == "/":
+    sum = int(x1) / int(x2)
+
+# 3. Output
+
+print(f"sum: {sum}")
