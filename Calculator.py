2953ea4f0a7d80bd9f7369931ab0ed85410e7b84
Max Yu <carlyou0815@gmail.com>
2024-03-25 11:55:46 +0800

    Revised

--- Calculator.py
+++ Calculator.py
@@ -1,15 +1,15 @@
 # Max Yu
 # Calulator to add two numbers and display the result
 
+print(f"Calculator")
 
 sum = 0 
 
 while True:
     # 1. Input
-    print(f"Calculator")
-    x1 = input("Enter first number: ")
+    x1 = input("Enter first number : ")
     x2 = input("Enter second number: ")
-    op = input("Enter operator: ")
+    op = input("Enter operator     : ")
 
     # 2. Process
     if op == "+":
@@ -22,7 +22,8 @@ while True:
         sum = int(x1) / int(x2)
 
     # 3. Output  
-    print(f"Sum: {sum}")
-    res = input("Continue? (Y/N)")
-    if res == "N":
+    print(f"Sum                : {sum}")
+    res = input("Continue? (Yes/No) : ")
+    if res == "No":
+        print(f"Exit")
         break;
